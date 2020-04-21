2196e1df8f9d32a7ed362f8f36625d69b85ddd54
Michael Irvine <michaelirvinedesign@gmail.com>
2020-04-21 16:34:15 -0700

    Finished Profile section

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
@@ -69,8 +70,11 @@ const Profile = ({
                 <h4>No education Credentials</h4>
               )}
             </div>
+
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
           </div>
-          Profile
         </Fragment>
       )}
     </Fragment>
